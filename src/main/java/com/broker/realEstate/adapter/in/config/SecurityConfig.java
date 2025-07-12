f85fb32a101fc9616654194d18893f185503436b
Sharunya <sharunyasr@gmail.com>
2025-07-12 19:17:09 +0530

    hexagonal architecture

--- src/main/java/com/broker/realEstate/adapter/in/config/SecurityConfig.java
+++ src/main/java/com/broker/realEstate/adapter/in/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.broker.realEstate;
+package com.broker.realEstate.adapter.in.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
