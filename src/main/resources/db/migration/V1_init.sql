107595e1f6dea64d543e9fdec78475a813e0b0f3
divyam <divyamgupta2013@gmail.com>
2025-07-12 19:44:08 +0530

    init file

---
+++ src/main/resources/db/migration/V1_init.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS 'users' (
+
+full_name varchar(50) NOT NULL,
+email varchar(50) PRIMARY KEY,
+password varchar(50) NOT NULL,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE IF NOT EXISTS 'users' (
+
+full_name varchar(50) NOT NULL,
+email varchar(50) PRIMARY KEY,
+password varchar(50) NOT NULL,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE IF NOT EXISTS 'users' (
+
+full_name varchar(50) NOT NULL,
+email varchar(50) PRIMARY KEY,
+password varchar(50) NOT NULL,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
