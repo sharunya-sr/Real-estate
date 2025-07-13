308c3885f56fe8e683bbb8b1293e6156cb5bbc58
Sharunya <sharunyasr@gmail.com>
2025-07-13 19:47:41 +0530

    h2 integration

--- src/main/java/com/broker/realEstate/adapter/in/config/SecurityConfig.java
+++ src/main/java/com/broker/realEstate/adapter/in/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,11 +14,10 @@ public class SecurityConfig {
 
     @Bean
     public  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception  {
-        http.csrf().disable()
-        .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-        .formLogin().disable()
-        .httpBasic().disable();
-return http.build();
+        return http.authorizeHttpRequests((requests)-> requests.anyRequest().permitAll())
+        .csrf(AbstractHttpConfigurer::disable)
+        .headers(headers->headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+        .build();
 
     }
 
--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS 'users' (
+CREATE TABLE IF NOT EXISTS users (
 
 full_name varchar(50) NOT NULL,
 email varchar(50) PRIMARY KEY,
