0aab3ece2c6d3c02c9eb5fa54ffc4dea3c966a96
divyam <divyamgupta2013@gmail.com>
2025-07-18 20:54:21 +0530

    register Api and exceptions

--- src/main/java/com/broker/realEstate/adapter/out/rest/exceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/broker/realEstate/adapter/out/rest/exceptionHandler/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.broker.realEstate.adapter.out.rest.exceptionHandler;
 
 import com.broker.realEstate.domain.exception.ConflictException;
+import com.broker.realEstate.domain.model.ErrorModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -14,19 +15,16 @@ import java.util.Map;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String,String>> handleValidationErrors(MethodArgumentNotValidException ex)
+    public ResponseEntity<ErrorModel> handleValidationErrors(MethodArgumentNotValidException ex)
     {
-        Map<String,String> errors = new HashMap<>();
-        ex.getBindingResult().getFieldErrors().forEach(err->
-        errors.put(err.getField(),
-                err.getDefaultMessage())
-        );
-        return ResponseEntity.badRequest().body(errors);
+        ErrorModel errorModel = new ErrorModel(HttpStatus.BAD_REQUEST.value(), ex.getBindingResult().getFieldError().getDefaultMessage());
+        return  new ResponseEntity<>(errorModel,HttpStatus.BAD_REQUEST);
+
     }
 
     @ExceptionHandler(ConflictException.class)
-    public ResponseEntity<String> handleConflictException(ConflictException ex)
-    {
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    public ResponseEntity<ErrorModel> handleConflictException(ConflictException ex)
+    {   ErrorModel errorModel = new ErrorModel(HttpStatus.CONFLICT.value(),ex.getMessage());
+        return new ResponseEntity<>(errorModel,HttpStatus.CONFLICT);
     }
 }
